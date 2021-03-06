11d91e4bb81c30f0ec636c7d0098a8dfcb854f22
JackRedstonia <jackredstonia64@gmail.com>
2021-03-06 14:01:15 +0700

    Add screenstack

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() {
             Paint::new_color4f(1.0, 1.0, 1.0, 1.0).anti_alias(),
         );
 
-        let root = VContainer::new(
+        let main_menu = VContainer::new(
             ContainerSize::ZERO.expand_width().expand_height(),
             None,
         )
@@ -41,16 +41,19 @@ fn main() {
         .with_child(logo)
         .with_child(Nothing::new(LayoutSize::ZERO.expand_height()));
 
-        let root = MarginContainer::new(Margin::all(12.0)).with_child(root);
+        let main_menu = MarginContainer::new(Margin::all(96.0)).with_child(main_menu);
 
         let background = Rectangle::new(
             LayoutSize::ZERO.expand_width().expand_height(),
             Paint::new_color4f(0.1, 0.3, 0.6, 1.0),
         );
-        let root = Backgrounded::new().with_child(background).with_child(root);
+        let main_menu = Backgrounded::new().with_child(background).with_child(main_menu);
+
+        let mut screens = ScreenStack::new();
+        screens.inner_mut().add_screen(main_menu);
 
         let mut intro = Intro::new();
-        intro.inner_mut().add_child(root);
+        intro.inner_mut().add_child(screens);
 
         // Set up fullscreening and auto-resizing
         let root = SizeFillContainer::new(Some(Size::new(1920.0, 1080.0)))
@@ -63,4 +66,115 @@ fn main() {
         Ok(root)
     })
     .expect("Failed to run game");
+}
+
+use stacks::game::ID;
+use std::collections::VecDeque;
+
+enum ScreenStackMessage {
+    Add(Wrap<dyn Widget>),
+    Remove(ID),
+}
+
+struct ScreenStackResource {
+    incoming_messages: VecDeque<ScreenStackMessage>,
+}
+
+impl ScreenStackResource {
+    pub fn add_screen<T: Widget + 'static>(&mut self, screen: Wrap<T>) {
+        self.incoming_messages
+            .push_back(ScreenStackMessage::Add(screen.to_dyn()));
+    }
+
+    pub fn remove_screen(&mut self, screen_id: ID) {
+        self.incoming_messages
+            .push_back(ScreenStackMessage::Remove(screen_id));
+    }
+}
+
+struct ScreenStack {
+    resource: ResourceHoster<ScreenStackResource>,
+    just_switched: bool,
+}
+
+impl ScreenStack {
+    pub fn new() -> Wrap<Self> {
+        let resource = ResourceHoster::new(ScreenStackResource {
+            incoming_messages: VecDeque::new(),
+        });
+        Self {
+            resource,
+            just_switched: false,
+        }
+        .into()
+    }
+
+    pub fn add_screen<T: Widget + 'static>(&mut self, screen: Wrap<T>) {
+        self.resource.access_mut().add_screen(screen);
+    }
+
+    pub fn remove_screen(&mut self, screen_id: ID) {
+        self.resource.access_mut().remove_screen(screen_id);
+    }
+}
+
+impl Widget for ScreenStack {
+    fn load(&mut self, state: &mut WidgetState, stack: &mut ResourceStack) {
+        stack.push(self.resource.new_user());
+        for i in state.children() {
+            i.load(stack);
+        }
+        stack.pop::<ResourceUser<ScreenStackResource>>();
+    }
+
+    fn update(&mut self, state: &mut WidgetState) {
+        let mut r = self.resource.access_mut();
+        while let Some(i) = r.incoming_messages.pop_front() {
+            match i {
+                ScreenStackMessage::Add(w) => {
+                    state.add_child_dyn(w);
+                }
+                ScreenStackMessage::Remove(_id) => {
+                    unimplemented!();
+                }
+            }
+        }
+
+        for i in state.children() {
+            i.update();
+        }
+    }
+
+    fn input(&mut self, state: &mut WidgetState, event: &InputEvent) -> bool {
+        state
+            .children()
+            .rev()
+            .next()
+            .map(|top| top.input(event))
+            .unwrap_or_default()
+    }
+
+    fn size(&mut self, state: &mut WidgetState) -> (LayoutSize, bool) {
+        let top_size = state
+            .children()
+            .rev()
+            .next()
+            .map(|top| top.size())
+            .unwrap_or_default();
+        (top_size.0, top_size.1 || self.just_switched)
+    }
+
+    fn set_size(&mut self, state: &mut WidgetState, size: Size) {
+        // TODO: should set size of from and to
+        if let Some(top) = state.children().rev().next() {
+            top.set_size(size);
+        }
+    }
+
+    fn draw(&mut self, state: &mut WidgetState, canvas: &mut Canvas) {
+        // TODO: should draw from and to
+        if let Some(top) = state.children().rev().next() {
+            top.draw(canvas);
+        }
+    }
 }
